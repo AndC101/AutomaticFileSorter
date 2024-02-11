8b8a8b857460e68943c8d535760c0fed7a513f86
Andrew Chen <andrew.chen7@student.tdsb.on.ca>
2024-02-11 12:48:37 -0500

    works

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
+#Andrew Chen
+#February 11 2024
+#Sorts your downloads folder based on keywords in the file name
+
 import os, shutil
 
-path = r"C:\Users\andre\Downloads\testfolder\\"
+path = r"C:\Users\andre\Downloads\\"
+
 print("\n\n")
 filename = os.listdir(path) #names of all files in the directory "path"
-print(filename);
+print(filename)
+
 #create folders for organization if not already made
-folder_names = ['Math Class', "Chemistry Class", "AP Physics C Class"]
-for loop in range(0, 3):
-    if not os.path.exists(path + folder_names[loop]):
-        print(path + folder_names[loop])
-        os.makedirs(path + folder_names[loop])
+# folder_names = ['Math Class', "Chemistry Class", "AP Physics C Class"]
+# for loop in range(0, 3):
+#     if not os.path.exists(path + folder_names[loop]):
+#         print(path + folder_names[loop])
+#         os.makedirs(path + folder_names[loop])
 
 for file in filename: 
-    if "Math" in file and not os.path.exists(path + "Math Class\\" + file) and not "Math Class" in file:
-        shutil.move(path+file, path +"Math Class\\"+file) 
-    elif "Chem" in file and not os.path.exists(path + "Chemistry Class\\" + file):
-        shutil.move(path+file, path +"Chemistry Class\\"+file) 
-    elif "APC" in file and not os.path.exists(path + "AP Physics C Class\\" + file):
-        shutil.move(path+file, path +"AP Physics C Class\\"+file) 
+    if "AP_IBHL_Chemistry" in file and not os.path.exists(path + "Chemistry\\" + file):
+        shutil.move(path+file, path +"Chemistry\\"+file) 
+    elif "_AP_Calculus" in file and not os.path.exists(path + "Calculus\\" + file):
+        shutil.move(path+file, path +"Calculus\\"+file) 
+    elif "PhysAPC" in file and not os.path.exists(path + "Physics C\\" + file):
+        shutil.move(path+file, path +"Physics C\\"+file) 
 
 print("\n\n")
